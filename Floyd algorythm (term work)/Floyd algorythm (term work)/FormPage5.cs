c3dfa38310f42fafa7c38355b0c2146cf887bfa7
Dima Buriak <buriak.dmytro1121@vu.cdu.edu.ua>
2022-12-14 11:01:01 +0200

    Commit-safe

--- Floyd algorythm (term work)/Floyd algorythm (term work)/FormPage5.cs
+++ Floyd algorythm (term work)/Floyd algorythm (term work)/FormPage5.cs
@@ -13,19 +13,41 @@ namespace Floyd_algorythm__term_work_
     public partial class FormPage5 : Form
     {
         private string matrixDataStrRow;
+        private int[,] matrixDataArray;
 
         public FormPage5(string matrixDataStrRow)
         {
             InitializeComponent();
+
             this.matrixDataStrRow = matrixDataStrRow;
+            FillMatrixDataArray();
+
+
+        }
+
+        private void FillMatrixDataArray()
+        {
+            string[] matrixDataStrRowsArray = matrixDataStrRow.Split('\n');
+
+            for (int i = 0; i < matrixDataStrRowsArray.Length; i++)
+            {
+                string[] matrixDataStrNumbersArray = matrixDataStrRowsArray[i].Split(' ');
+
+                for (int j = 0; j < matrixDataStrNumbersArray.Length; j++)
+                {
+                    string numberStr = matrixDataStrNumbersArray[j].Trim(' ');
+
+                    matrixDataArray[i, j] = int.Parse(numberStr);
+                }
+            }
         }
 
         private void FormPage5_Load(object sender, EventArgs e)
         {
             Label lbNew = new Label();
-            lbNew.Text = matrixDataStrRow /* "lbNew" */;
+            lbNew.Text = matrixDataArray[1, 1].ToString();
             PanelInitialMatrixData.Controls.Add(lbNew);
-            toolTipShowShortestPath.SetToolTip(lbNew, "lbNew");
+            toolTipShowShortestPath.SetToolTip(lbNew, "[1, 1]");
         }
     }
 }
